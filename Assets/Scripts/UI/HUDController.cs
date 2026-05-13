bebf442b5146a4926e95a7e0fda887920682fa25
Metin Enes Ufuk <metinenes76@gmail.com>
2026-05-13 10:15:52 +0300

    add HUDController
    feat: add HUDController subscribing to GameEventBus events

---
+++ Assets/Scripts/UI/HUDController.cs
@@ -0,0 +1,42 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDController : MonoBehaviour
+{
+    [SerializeField] private Image coreHealthFill;
+    [SerializeField] private TextMeshProUGUI waveText;
+    [SerializeField] private TextMeshProUGUI killText;
+
+    private void OnEnable()
+    {
+        GameEventBus.OnCoreDamaged   += HandleCoreDamaged;
+        GameEventBus.OnEnemyDied     += HandleEnemyDied;
+        GameEventBus.OnWaveCompleted += HandleWaveCompleted;
+    }
+
+    private void OnDisable()
+    {
+        GameEventBus.OnCoreDamaged   -= HandleCoreDamaged;
+        GameEventBus.OnEnemyDied     -= HandleEnemyDied;
+        GameEventBus.OnWaveCompleted -= HandleWaveCompleted;
+    }
+
+    private void HandleCoreDamaged(float current, float max)
+    {
+        if (coreHealthFill != null)
+            coreHealthFill.fillAmount = current / max;
+    }
+
+    private void HandleEnemyDied(int killCount)
+    {
+        if (killText != null)
+            killText.text = $"Kills: {killCount}";
+    }
+
+    private void HandleWaveCompleted(int waveIndex)
+    {
+        if (waveText != null)
+            waveText.text = $"Wave: {waveIndex}";
+    }
+}
