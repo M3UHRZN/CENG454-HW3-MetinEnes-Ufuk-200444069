e51ae4aa48e325629284a4a096fb8b4864ffca4c
Metin Enes Ufuk <metinenes76@gmail.com>
2026-05-15 06:04:33 +0300

    add EnemyWeapon MonoBehaviour implementing IWeapon

---
+++ Assets/Scripts/Enemies/EnemyWeapon.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class EnemyWeapon : MonoBehaviour, IWeapon
+{
+    [SerializeField] private float cooldown   = 2f;
+    [SerializeField] private float fireHeight = 1f;
+
+    public float Cooldown => cooldown;
+
+    private float _timer;
+
+    private void Update()
+    {
+        if (_timer > 0f)
+            _timer -= Time.deltaTime;
+    }
+
+    public void Fire(Vector3 direction)
+    {
+        if (_timer > 0f) return;
+        _timer = cooldown;
+
+        Vector3 origin = transform.position + Vector3.up * fireHeight;
+        BulletPool.Instance.Launch(origin, direction, gameObject.name);
+    }
+}
